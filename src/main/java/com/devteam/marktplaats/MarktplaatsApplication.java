ca92a66db1e0059e09f8f0bc074558f13415e700
Martin <martin_de_v@hotmail.com>
2023-12-06 11:43:52 +0100

    CORS error fix

--- src/main/java/com/devteam/marktplaats/MarktplaatsApplication.java
+++ src/main/java/com/devteam/marktplaats/MarktplaatsApplication.java
@@ -2,8 +2,10 @@ package com.devteam.marktplaats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("nl.youngcapital.marktplaats")
 public class MarktplaatsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/nl/youngcapital/marktplaats/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package nl.youngcapital.marktplaats.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // Add your frontend origin(s) here
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
